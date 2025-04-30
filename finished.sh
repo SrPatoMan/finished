240992441771fe5415435e732c0eea4706d04ebe
SrPatoMan 🦆 <85370678+SrPatoMan@users.noreply.github.com>
2025-04-30 02:54:38 +0200

    Script funcionando con webhook de discord correctaente

--- finished.sh
+++ finished.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 #By Manuel Ramos
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-
 WEBHOOK_DISCORD="" # PON AQUI TU WEBHOOK DE DISCORD
+
 WEBHOOK_TELEGRAM="" # PON AQUI TU WEBHOOK DE TELEGRAM
-PLATFORM="" # PONER telegram o discord (segun la plataforma que uses)
+
+PLATFORM="discord" # PONER telegram o discord (segun la plataforma que uses)
 
 "$@"
 exit_code=$?
-
+herramienta=$1
 
 if [ $exit_code -eq 0 ];then
-mensaje="${GREEN}[+] El proceso ha finalizado con exito"
+mensaje="[+] $herramienta ha finalizado con exito"
 else
-mensaje="${RED}[-] El proceso ha finalizado de manera no exitosa"
+mensaje="[-] $herramienta ha finalizado de manera no exitosa"
 fi
 
-echo -e $message
 
-if [ $PLATFORM -eq 'discord' ];then
-curl -X POST -H 'Content-Type: application/json' -d '{"content": "$mensaje"}' "$WEBHOOK_DISCORD"
+if [ $PLATFORM == 'discord' ];then
+curl -X POST -H 'Content-Type: application/json' -d "{\"content\": \"$mensaje\"}" "$WEBHOOK_DISCORD"
 fi
 
-if [ $PLATFORM -eq 'telegram' ];then
+#if [ $PLATFORM == 'telegram' ];then
 ## PONER PETICION CURL AL WEBHOOK DE TELEGRAM
-fi
+#fi
