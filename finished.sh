3197aa28d215475e3836a3e0af36d815c0e59be4
SrPatoMan 🦆 <85370678+SrPatoMan@users.noreply.github.com>
2025-05-02 02:54:52 +0200

    finished.sh acabado y funcionando tanto en Discord como en Telegram

--- finished.sh
+++ finished.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #By Manuel Ramos
 
-WEBHOOK_DISCORD="" #PON AQUI TU WEBHOOK DE DISCORD
+## DISCORD (Rellenar si usas Discord) ##
+WEBHOOK_DISCORD=""
 
-WEBHOOK_TELEGRAM="" # PON AQUI TU WEBHOOK DE TELEGRAM
+## TELEGRAM (Rellenar si usas Telegram)##
+BOT_TOKEN=""
+CHAT_ID=""
 
-PLATFORM="discord" # PONER telegram o discord (segun la plataforma que uses)
+PLATFORM="discord" # PONER telegram o discord (segun la plataforma que uses, por defecto discord)
 
-if [[ -z "$WEBHOOK_DISCORD" && -z "$WEBHOOK_TELEGRAM" ]];then
-echo -e "\n[!] NO HAS ESTABLECIDO NINGUN WEBHOOK!!\n"
-echo -e "[+] Abre el script e introduce tu webhook de telegram o discord"
+if [[ -z "$WEBHOOK_DISCORD" && -z "$BOT_TOKEN" ]];then
+echo -e "\n[!] NO HAS ESTABLECIDO NINGUN WEBHOOK DE DISCORD O BOT DE TELEGRAM!!\n"
+echo -e "[+] Abre el script e introduce tu webhook de telegram o tu bot discord"
 exit
 fi
 
@@ -18,9 +21,9 @@ exit_code=$?
 herramienta=$1
 
 if [ $exit_code -eq 0 ];then
-mensaje="[+] $herramienta ha finalizado con exito"
+mensaje="[+] |   $herramienta   | ha finalizado con exito"
 else
-mensaje="[-] $herramienta ha finalizado de manera no exitosa"
+mensaje="[-] |   $herramienta   | ha finalizado de manera no exitosa"
 fi
 
 
@@ -28,6 +31,6 @@ if [ $PLATFORM == 'discord' ];then
 curl -X POST -H 'Content-Type: application/json' -d "{\"content\": \"$mensaje\"}" "$WEBHOOK_DISCORD"
 fi
 
-#if [ $PLATFORM == 'telegram' ];then
-## PONER PETICION CURL AL WEBHOOK DE TELEGRAM
-#fi
+if [ $PLATFORM == 'telegram' ];then
+curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$mensaje" >/dev/null 2>&1
+fi
